1dbde7ab2a10806376f36823200e7f98eff8b457
tpob <tpob@tpob.xyz>
2022-03-07 01:55:59 +0800

    thoughts about how I should implement the thing

--- bdg_operation/libs/map.py
+++ bdg_operation/libs/map.py
@@ -11,6 +11,14 @@ def map_bdg_on_bed(bed_file: BedFile, bdg_file: BdgFile, genome_size: GenomeSize
     for bed_entry in bed_file:
         for bdg_entry in BdgFile:
             if bed_entry[0] != bdg_entry[0]:
+                # here is a problem need to be done, chromosome switching, I need to find a way to switch chromosomes
+                # maybe I can let the two loop compare with their last entry
+                # here is two situations:
+                # 1. bedgraph on chr2, but bed has no chr2, in this case I need to make bdg loop(inner loop) "continue"
+                # 2. bed on chr2, but bedgraph has no chr2, if I let bedgraph continue directly, bed will stuck on this entry
+                # oh I think I figured it out, the goal is the map bedgraph scores to bed, so I need only to consider bed entry
+                # when they have different chromosomes, I can "compare" them using the default sort method, and determine if I
+                # should score this region as 0
                 break
 
             
